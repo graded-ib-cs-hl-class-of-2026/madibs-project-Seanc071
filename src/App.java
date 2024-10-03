e9f75232d805df1f4936c85c0ea859a78408e2a0
Sean <No>
2024-10-03 19:44:20 -0300

    Finish

--- src/App.java
+++ src/App.java
@@ -1,8 +1,12 @@
-import java.util.Scanner;
+/**To add scanner in my coding, I imported Scanner
+ *  I find out this from 
+ * https://www.w3schools.com/java/java_user_input.asp
+ * */
+import java.util.Scanner;//import the scanner
 
 public class App {
-    Scanner scan = new Scanner(System.in); 
-    int mode;//as the mode should be 1, 2, or 3
+    Scanner scan = new Scanner(System.in); //Creates a scanner to read input from the user
+    int mode;//This varable will store the numbers in integer
     
     
     /**
@@ -12,40 +16,47 @@ public class App {
      */
 //start
     public void start() {
-        String userName = getUserName();
-        printGreeting(userName);
-        printInstructions();
-        playGame();
+        String userName = getUserName();//get user's name
+        printGreeting(userName);//greet the user
+        printInstructions();//provide instructions on how to play
+        playGame();//start the game process
     }
-    /* Here you should create all missing methods used by the start() method
-     * I have created the signature for ONE of the methods below.
-     * You can create any other methods you need as well!
-     */ 
-    
+ 
+    /**This method handles the entire game flow
+     * It asks the user to choose a mode, confirms their choice, and play the game
+     * and, it asks if users would like to paly again
+    */
     public void playGame(){
-        //order of the game progress
-        chooseMode();
-        confirm();
-        doMadlib();
-        again();
+        chooseMode();//user selects a mdoe(1,2, or 3)
+        confirm();// Confirm user's mode selction
+        doMadlib();//Generate the madelib story based on user input
+        again();// Ask if the user wants to play again
         
         
 
     }
-   //get username
+   /**This method lets the user to enter their name
+    * First it asks to input the name, and give a space to put ur name 
+    by using scanner.
+    *Then, it return the name as a string
+    */
     public String getUserName() {
         System.out.println("Please enter your name");
         String name;
         name = scan.nextLine();
         return name;
     }
-//greeting
-    public void printGreeting(String userName) {
+/**This method prints a greeting message to the user using their name
+ * it uses the user's name entered previously
+ */
+    public void printGreeting(String name) {
       
-        System.out.println("Hello "+ userName + "! Welcome to the Mad Libs game.");
+        System.out.println("Hello "+ name + "! Welcome to the Mad Libs game.");
        
     }
-     /** Prints instructions */
+     /** this method provides instructions to the user, explaing how the
+      * game works
+     */
     public void printInstructions() {
 
 
@@ -53,7 +64,7 @@ public class App {
         System.out.println("once you've provided the words, fun story will come out with your words!");
         System.out.println("Now let's get started!");}
          
-        //choosing the mode
+      //** This method makes the uswer to choose a mode for the game.*/
         public void chooseMode(){
         System.out.println("Choose a mode");
           System.out.println("mode 1: Jungle exploring");
@@ -61,31 +72,39 @@ public class App {
           System.out.println("mode 3: Desert exploring");
           System.out.println("Enter mode 1,2 or 3");
 
-       
+       /** 
+        * https://docs.google.com/document/d/15cSsXSbyq7P4n614_HGMQOjGWnXu_1e1-TiqNMuv4bs/edit?usp=sharing
+      So the while(true) makes a infite loop until the player answers correctly
+        */
+    
           while (true){
            
-//https://www.youtube.com/watch?v=1XAfapkBQjk
-//stackoverflow.com/questions/19799838/only-allow-input-of-integers-with-java-scanner
+/**https://www.youtube.com/watch?v=1XAfapkBQjk
+ * learned from this link.
+*/
                 try{
+                    //only gets an ingeger input from the user
                     mode = Integer.parseInt(scan.nextLine());
                   
-                    if(mode >= 1 && mode <=3){
-                        break;
+                    //Check if the input is in the range 1 to 3
+                  if(mode >= 1 && mode <=3){
+                        break;//if the answer is between them, exit the loop
+                    //If the number isn't in range 1-3, print a message and repeat the loop
                     }else{
                         System.out.println("Invalid number. please enter 1,2 or 3.");}}
-                        
+                        //if the input isn't a ingteger number, print this message
                         catch (NumberFormatException nfe) {
                             System.out.println("Invalid input. Please enter a number.");
-                        //code that i need to exexute if this type of exception
+                       
                     }
                     }}
     
     
-    //confirm the choice
+  //**This method asks the user to confirm the mode they chose.It gives them the option to either continue or change the mode */
  public void confirm(){
 
         String answer;
-
+//made a infinte loop,until I get the answer I want.
         while (true){
         System.out.println("If you want to continue with mode " + mode +" type yes or not, type no");
         answer = scan.nextLine();
@@ -95,9 +114,8 @@ public class App {
            break; //exit the loop
         } else if(answer.equals("no") ) {
             System.out.println("Pick another mode!");
-            
            }
-
+           //If player type 'yes' it will exit the loop, but if player type 'no' player will enter other loop
             while (true) {
                 try {
                 mode = Integer.parseInt(scan.nextLine());
@@ -106,8 +124,8 @@ public class App {
                     else{
                        System.out.println("Invalid number. Please enter 1,2, or 3.");      
                     }
-                } catch(NumberFormatException e) {
-                    System.err.println("Invalid input. Please enter a number.");
+                } catch(NumberFormatException nfe) {
+                    System.out.println("Invalid input. Please enter a number.");
                 }
             }
         }
@@ -120,11 +138,14 @@ public class App {
 
 
 
+//**This method is generating the madlib stories based on the player's chosen mode. It makes players to fill the blanks of the story */
 
     //stories of the madlib depend on case 1, 2, 3
     public void doMadlib() {
 //https://www.youtube.com/watch?v=wcwWlasmLWs
-        switch(mode){
+//used a swith(mode) to play story based on player's choice previously
+       //if player choose mode 1
+switch(mode){
      case 1: 
 
         String adjective1;
@@ -143,7 +164,7 @@ public class App {
         String verbEndingInIng;
         String food;
         String adjective5;
-
+//let player to choose the word
         System.out.print("Enter a adjective 1:  ");
         adjective1 = scan.nextLine();
         System.out.print("Enter a name:  ");
@@ -179,7 +200,7 @@ public class App {
         
      
         
-
+       //Add the words that player wrote previously
         String story1 = "Once upon a time, in a " + adjective1 + " jungle, there was a explorer named " + name + " who loved to " +
          verb1 + " every day. One day, while exploring hidden cave, he stumbled upon a " + adjective2 + " treasure chest filled with " + 
          nounPlural + ". Excitedly, he grabbed " + number +  " of them and ran outside. But suddenly, a giant " + animal + " appeared and started to " + 
@@ -188,9 +209,9 @@ public class App {
            ". He celebrated the escape with his family by " + verbEndingInIng + " and eating " + food + " for dinner. It was truly a " + adjective5 +" day!";
         System.out.print(story1);
         break;
+        //exit the method
         
-        
-    
+    //If player choose mode 2
         case 2:
 
         String name2;
@@ -205,6 +226,7 @@ public class App {
         String verbEndingInIng2;
         String food2;
         String number2;
+//let player to choose the word
 
         System.out.print("Enter a name :  ");
         name2 = scan.nextLine();
@@ -234,7 +256,7 @@ public class App {
 
 
 
-       
+       //Add the words that player wrote previously
       String story2 ="One day, " + name2 + " decided to go on an " + adjective6 + " ocean expedition. " +
       "While diving near " + place + ", they encountered a huge " + seaCreature + " that looked very " + adjective7 + ". " +
                     name3 + " quickly " + verb4 + " and hid behind a " + noun1 + ". As they watched, more " + nounPlural2 + 
@@ -243,7 +265,7 @@ public class App {
                     "and the crew counted " + number2 + " sea creatures in total!";
                     System.out.println(story2); break;
                     
-    
+    //exit the method
 
                     case 3: 
 
@@ -260,6 +282,8 @@ public class App {
                     String activity;
                     String food3;
                     String number3;
+                    //let player to choose the word
+
                     System.out.print("Enter a exploer name :  ");
                     explorerName = scan.nextLine();
                     System.out.print("Enter a adjective :  ");
@@ -286,6 +310,7 @@ public class App {
                     food3 = scan.nextLine();
                     System.out.print("Enter a number :  ");
                     number3 = scan.nextLine();
+       //Add the words that player wrote previously
 
                     String story3;
                      story3 = "Once upon a time, " + explorerName + " set out on an adventurous journey through the " + adjective8 + 
@@ -295,17 +320,22 @@ public class App {
                       " spent the evening " + activity + "and enjoyed a delicious meal of " + food3 + " with " + number3 + " friends.";  
                       System.out.println(story3);
                       break;
+    //exit the method
 
 
                         }}
 
-// playing agian
+/**This method asks if the players liked the game.
+ * If they say no, game asks the feedback and asks if they wants to play again
+ * If they say yes, game just asks if they want to play again
+ * 
+ */
                 public void again() {
                     String answer2;
                     String feedback;
                     String answer3;
                     String answer4;
-                    
+                    //created a infinite loop until player write a correct answer
                     while(true){
                         System.out.println("Did you like the game?(yes/no)");
                         answer2 = scan.nextLine();
@@ -328,7 +358,7 @@ public class App {
                             feedback = scan.nextLine();
                             System.out.println("Thank you for the feedback! do you want to replay this game? (yes/no)");
                             answer3 = scan.nextLine();
-                           
+                           //If player wants to play again, go back to method playGame() and restart the game from there
                             if(answer3.equals("yes")){
                             playGame(); break;}
                                 else if(answer3.equals("no")){
